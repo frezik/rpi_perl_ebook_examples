eea5f5d20ff04f3ec559aea6e5e9704fc939fbe1
tmurray <tmurray@wumpus-cave.net>
2018-09-24 16:29:22 -0500

    Some bugfixes

--- async_input.pl
+++ async_input.pl
@@ -5,17 +5,10 @@ use RPi::Const qw(:all);
 
 my $pi = RPi::WiringPi->new;
 my $pin2 = $pi->pin( 2 );
-my $pin3 = $pi->pin( 3 );
-my $pin4 = $pi->pin( 4 );
 
-foreach ($pin2, $pin3, $pin4) {
-    $_->mode( INPUT );
-    $_->pull( PUD_UP );
-}
-
-$pin2->set_interrupt( EDGE_RISING, 'input_handler_2' );
-$pin3->set_interrupt( EDGE_FALLING, 'input_handler_3' );
-$pin4->set_interrupt( EDGE_BOTH, 'input_handler_4' );
+$pin2->mode( INPUT );
+$pin2->pull( PUD_UP );
+$pin2->set_interrupt( EDGE_RISING, 'input_handler' );
 
 
 say "Awaiting input . . . ";
@@ -24,17 +17,7 @@ while(1) {
 }
 
 
-sub input_handler_2
+sub input_handler
 {
     say "Pin 2 triggered";
-}
-
-sub input_handler_3
-{
-    say "Pin 3 triggered";
-}
-
-sub input_handler_4
-{
-    say "Pin 4 triggered";
 }
--- base_packages.sh
+++ base_packages.sh
@@ -1,5 +1,8 @@
 apt-get install wiringpi cpanminus libperl-dev libgdbm-dev libdb-dev \
     libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
-    gstreamer1.0-plugins-ugly gobject-introspection gir1.2-gst-plugins-bad1.0 \
-    gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 \
-    libglib-object-introspection-perl
+    gstreamer1.0-plugins-ugly gobject-introspection gir1.2-gst-plugins-bad-1.0 \
+    gstreamer1.0-tools gir1.2-gst-plugins-base-1.0 gir1.2-gstreamer-1.0 \
+    libglib-object-introspection-perl \
+    autoconf automake libtool pkg-config \
+    libgstreamer-plugins-base1.0-dev libraspberrypi-dev git \
+    opencv-data
